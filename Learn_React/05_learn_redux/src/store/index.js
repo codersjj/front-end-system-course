50fe0de91ceff5ba9eb6b2f5b5221007f71700ed
codersjj <shanewestlife@outlook.com>
2023-07-19 21:58:23 +0800

    feat(day85/Learn_React/05_learn_redux): Redux-index.js 文件中的 reducer 内容抽取

--- Learn_React/05_learn_redux/src/store/index.js
+++ Learn_React/05_learn_redux/src/store/index.js
@@ -1,27 +1,6 @@
 const { createStore } = require('redux')
-const { CHANGE_NAME, ADD_NUMBER } = require('./constants')
 
-// 初始化的数据
-const initialState = {
-  name: 'jack',
-  counter: 100
-}
-
-// 定义 reducer 函数（要求是纯函数）
-// 两个参数：
-// 参数一：store 中当前保存的 state，这里我们给它一个初始值，否则第一次拿到的是 undefined
-// 参数二：本次需要更新的 action（dispatch 传入的 action）
-// 返回值：会作为 store 之后要存储的 state
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_NAME:
-      return { ...state, name: action.name }
-    case ADD_NUMBER:
-      return { ...state, counter: state.counter + action.num }
-    default:
-      return state
-  }
-}
+const reducer = require('./reducer')
 
 // 创建的 store
 const store = createStore(reducer)
