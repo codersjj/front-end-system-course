8d8154566845f3cfb4392bdf84b03953ae63dfd8
codersjj <shanewestlife@outlook.com>
2023-07-22 16:41:43 +0800

    feat(day86/Learn_React/07_learn_redux_toolkit): Redux-RTK 的基本使用

---
+++ Learn_React/07_learn_redux_toolkit/src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class Profile extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h2>Profile page counter: 0</h2>
+      </div>
+    )
+  }
+}
+
+export default Profile
