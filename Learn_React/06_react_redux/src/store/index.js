2b3cce3ce197917953d736915e33befff8738af2
codersjj <shanewestlife@outlook.com>
2023-07-23 23:36:11 +0800

    feat(day87/Learn_React/06_react_redux): Redux-实现打印日志的中间件

--- Learn_React/06_react_redux/src/store/index.js
+++ Learn_React/06_react_redux/src/store/index.js
@@ -48,4 +48,22 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: tr
 // 要想派发函数：store.dispatch(function)，需要在 createStore 时进行增强
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
 
+// 对每次派发的 action 进行拦截，做日志打印
+function log(store) {
+  const next = store.dispatch
+
+  function logAndDispatch(action) {
+    console.log('当前派发的 action：', action)
+    // 真正派发的代码，使用之前的 dispatch 进行派发
+    next(action)
+    console.log('派发之后的 state：', store.getState())
+  }
+
+  // monkey patch：猴补丁，是一种在不改变原始源代码的情况下扩展或修改动态语言(如Smalltalk、JavaScript、Objective-C、Ruby、Perl、Python、Groovy等)运行时代码的方式。
+  store.dispatch = logAndDispatch
+}
+log(store)
+
+// store.dispatch({ type: 'add_number', num: 100 })
+
 export default store
