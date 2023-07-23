b257099c203fc415ffb5a47a123120fe743bce35
codersjj <shanewestlife@outlook.com>
2023-07-23 18:23:11 +0800

    feat(day86/Learn_React/07_learn_redux_toolkit): Redux-react-redux 中 connect 函数的解构操作

--- Learn_React/07_learn_redux_toolkit/src/hoc/connect.js
+++ Learn_React/07_learn_redux_toolkit/src/hoc/connect.js
@@ -4,28 +4,30 @@
 // 返回值：函数（并且是一个高阶组件）
 
 import { PureComponent } from "react"
-import store from "../store"
+// import store from "../store"
+import { StoreContext } from "./StoreContext"
 
 export function connect(mapStateToProps, mapDispatchToProps) {
   // 返回一个高阶组件（函数）
   return function(WrappedComponent) {
     class NewComponent extends PureComponent {
-      constructor(props) {
+      constructor(props, context) {
+        // console.log('constructor args', arguments)
         super(props)
 
         // 通过执行 mapStateToProps(store.getState())，获取只在该组件中需要用到的数据
-        this.state = mapStateToProps(store.getState())
+        this.state = mapStateToProps(context.getState())
       }
 
       componentDidMount() {
         // 通过订阅，监听数据的改变
-        this.unsubscribe = store.subscribe(() => {
+        this.unsubscribe = this.context.subscribe(() => {
           // 强制重新渲染（不建议，因为我们可能只用了一部分数据，没有必要将所有的数据都刷新）
           // this.forceUpdate()
 
           // 通过执行 mapStateToProps(store.getState())，获取只在该组件中用到的数据，再重新 setState
           // 每次调用 mapStateToProps(store.getState()) 都会返回一个新对象，而 PureComponent 底层会判断是否是 shallowEqual，oldState 和 newState 不是同一个对象时，之后才会去 re-render
-          this.setState(mapStateToProps(store.getState()))
+          this.setState(mapStateToProps(this.context.getState()))
         })
       }
 
@@ -36,11 +38,13 @@ export function connect(mapStateToProps, mapDispatchToProps) {
 
       render() {
         console.log('render ~')
-        const stateObj = mapStateToProps(store.getState())
-        const dispatchObj = mapDispatchToProps(store.dispatch)
+        const stateObj = mapStateToProps(this.context.getState())
+        const dispatchObj = mapDispatchToProps(this.context.dispatch)
         return <WrappedComponent {...this.props} {...stateObj} {...dispatchObj} />
       }
     }
+
+    NewComponent.contextType = StoreContext
 
     return NewComponent
   }
--- Learn_React/07_learn_redux_toolkit/src/index.js
+++ Learn_React/07_learn_redux_toolkit/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux'
+import { StoreContext } from './hoc'
 import App from './App';
 import store from './store'
 
@@ -8,7 +9,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
     <Provider store={store}>
-      <App />
+      {/* 实现和上面 react-redux 中 Provider 一样的基本功能 */}
+      <StoreContext.Provider value={store}>
+        <App />
+      </StoreContext.Provider>
     </Provider>
   // </React.StrictMode>
 )
