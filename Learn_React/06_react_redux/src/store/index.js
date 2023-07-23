25ad7aac9b750de85a19122a88bd0405a3893bc6
codersjj <shanewestlife@outlook.com>
2023-07-24 07:46:00 +0800

    feat(day87/Learn_React/06_react_redux): Redux-实现 redux-thunk 的核心逻辑

--- Learn_React/06_react_redux/src/store/index.js
+++ Learn_React/06_react_redux/src/store/index.js
@@ -1,10 +1,15 @@
-import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import {
+  createStore,
+  // applyMiddleware,
+  // compose,
+  combineReducers
+} from "redux";
 
 import counterReducer from './counter'
 import homeReducer from './home'
 import userReducer from './user'
 
-import thunk from "redux-thunk";
+// import thunk from "redux-thunk";
 
 const reducer = combineReducers({
   counter: counterReducer,
@@ -42,11 +47,11 @@ function reducer(state = {}, action) {
 // redux devtools 开启配置
 // const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 // 开启 redux devtools 中的 Trace 功能
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ trace: true }) || compose;
 
 // 正常情况下，store.dispatch(object)
 // 要想派发函数：store.dispatch(function)，需要在 createStore 时进行增强
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+const store = createStore(reducer)
 
 // 对每次派发的 action 进行拦截，做日志打印
 function log(store) {
@@ -65,5 +70,21 @@ function log(store) {
 log(store)
 
 // store.dispatch({ type: 'add_number', num: 100 })
+
+function thunk(store) {
+  const next = store.dispatch
+
+  function dispatchThunk(action) {
+    if (typeof action === 'function') {
+      // 函数中可能会继续出现 dispatch(function) 的情况，所以这里传给 action 的第一个参数是 store.dispatch，而不是 next
+      action(store.dispatch, store.getState)
+    } else {
+      next(action)
+    }
+  }
+
+  store.dispatch = dispatchThunk
+}
+thunk(store)
 
 export default store
