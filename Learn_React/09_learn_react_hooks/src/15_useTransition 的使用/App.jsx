04be83e01b4cb54cb8ec6910ab2d72e1564d68ba
codersjj <shanewestlife@outlook.com>
2023-08-06 21:39:33 +0800

    feat(day89/Learn_React/09_learn_react_hooks): Hooks-useTransition 的使用过程

--- Learn_React/09_learn_react_hooks/src/15_useTransition 的使用/App.jsx
+++ Learn_React/09_learn_react_hooks/src/15_useTransition 的使用/App.jsx
@@ -1,20 +1,29 @@
-import React, { memo, useState } from 'react'
+import React, { memo, useState, useTransition } from 'react'
 import namesArray from './namesArray'
 
 const App = memo(() => {
   const [showNames, setShowNames] = useState(namesArray)
+  const [pending, startTransition] = useTransition()
 
   function handleInput(e) {
-    const { value: keyword } = e.target
-    // 注意：这里需要从完整的数组即 namesArray 中进行过滤
-    const filteredShowNames = namesArray.filter(name => name.includes(keyword))
-    setShowNames(filteredShowNames)
+    // 告诉 react 对于某部分任务的更新优先级较低，可以稍后进行更新
+    startTransition(() => {
+      const { value: keyword } = e.target
+      // 注意：这里需要从完整的数组即 namesArray 中进行过滤
+      const filteredShowNames = namesArray.filter(name => name.includes(keyword))
+      setShowNames(filteredShowNames)
+    })
   }
 
   return (
     <div>
+      {/*
+        这里文本框中输入（或删除）之后文本的渲染和下面列表内容的渲染是在同一次更新中进行的。
+        但是要渲染的列表内容很多时，浏览器可能会出现卡顿，直观的效果就是输入（或删除）之后文本的渲染结果和下面列表内容的渲染结果会等待一定时间后再同时呈现。
+        为了更明显地看到卡顿效果，我们可以模拟电脑硬件较差时浏览器的渲染效果：F12 -> Performance -> 右上角图标（Capture settings -> CPU 选择 4x slowdown）
+      */}
       <input type="text" onInput={handleInput} />
-      <h2>用户名列表：</h2>
+      <h2>用户名列表：{pending && <span>loading...</span>}</h2>
       <ul>
         {
           showNames.map((name, index) => {
--- Learn_React/09_learn_react_hooks/src/15_useTransition 的使用/namesArray.js
+++ Learn_React/09_learn_react_hooks/src/15_useTransition 的使用/namesArray.js
@@ -1,7 +1,7 @@
 import { faker } from '@faker-js/faker'
 
 // const namesArray = Array.from(Array(1000)).map(item => faker.person.fullName())
-const namesArray = Array.from(Array(1000))
+const namesArray = Array.from(Array(10000))
   .map(item => faker.helpers.unique(faker.person.fullName))
 
 export default namesArray
