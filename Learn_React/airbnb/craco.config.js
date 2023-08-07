2577b234bd875b0bc35e9095f2fe12d26f313dc9
codersjj <shanewestlife@outlook.com>
2023-08-07 16:43:44 +0800

    feat(day89/Learn_React/airbnb): 项目搭建-craco 配置路径别名和 less 样式插件

---
+++ Learn_React/airbnb/craco.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const CracoLessPlugin = require("craco-less");
+
+/**
+ * Resolve path for a project.
+ * @param {string} filePath Relative path from project root
+ * @returns {string} The resolved absolute path.
+ */
+const resolve = filePath => path.resolve(__dirname, filePath)
+
+module.exports = {
+  // webpack
+  webpack: {
+    alias: {
+      '@': resolve('src'),
+      'components': resolve('src/components'),
+      'utils': resolve('src/utils')
+    }
+  },
+  plugins: [
+    // less
+    {
+      plugin: CracoLessPlugin
+    }
+  ]
+}
--- Learn_React/airbnb/src/index.js
+++ Learn_React/airbnb/src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import './assets/css/index.less'
+
+// @ => src: 在 Webpack 中进行配置
+// 问题：react 脚手架（create-react-app）隐藏了 Webpack 的配置
+// 解决方案一：npm run eject
+// （推荐）解决方案二：craco -> create react app configuration override
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
