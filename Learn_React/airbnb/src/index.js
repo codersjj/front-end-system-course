dd6ce5f59842f2e96cf613fc1e842cb0d64e37af
codersjj <shanewestlife@outlook.com>
2023-08-08 12:15:34 +0800

    feat(day89/Learn_React/airbnb): 项目搭建-项目状态管理 redux 的配置

--- Learn_React/airbnb/src/index.js
+++ Learn_React/airbnb/src/index.js
@@ -1,10 +1,12 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 import 'normalize.css'
 import './assets/css/index.less'
+import store from './store';
 
 // @ => src: 在 Webpack 中进行配置
 // 问题：react 脚手架（create-react-app）隐藏了 Webpack 的配置
@@ -16,7 +18,9 @@ root.render(
   <React.StrictMode>
     <Suspense fallback='loading'>
       <HashRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </HashRouter>
     </Suspense>
   </React.StrictMode>
