6210298c4c139755392429f6edf5ee0d743b338b
codersjj <shanewestlife@outlook.com>
2023-08-09 09:54:59 +0800

    feat(day90/Learn_React/airbnb): 项目-App 整体架构和 Header 的不同实现方案 Header 实现方案一：只用一个组件实例实现，实例内部根据不同页面 Header 的不同样式，进行不同的处理； Header 实现方案二：使用多个组件实例实现，在不同的页面中使用自己的 Header 组件实例； 我们这里采用方案一

--- Learn_React/airbnb/src/views/home/index.jsx
+++ Learn_React/airbnb/src/views/home/index.jsx
@@ -1,38 +1,9 @@
-import React, { memo, useEffect, useState } from 'react'
-import jjRequest from '@/services'
+import React, { memo } from 'react'
 
 const Home = memo(() => {
-  // 定义状态
-  const [highScore, setHighScore] = useState({})
-
-  const { title, subtitle, list } = highScore
-
-  // 网络请求的代码
-  useEffect(() => {
-    jjRequest.get('/home/highscore')
-      .then(res => {
-        console.log(res)
-        setHighScore(res)
-      })
-  }, [])
-
   return (
     <div>
-      <h2>Home</h2>
-      <h3>{title}</h3>
-      <h4>{subtitle}</h4>
-      <ul>
-        {/* {
-          list && list.map(item => {
-            return <li key={item.id}>{item.name}</li>
-          })
-        } */}
-        {
-          list?.map(item => {
-            return <li key={item.id}>{item.name}</li>
-          })
-        }
-      </ul>
+      <h2>Home page</h2>
     </div>
   )
 })
