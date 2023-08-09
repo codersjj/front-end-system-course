fbd23c86431aa15032b82cb31cf213238f576dec
codersjj <shanewestlife@outlook.com>
2023-08-09 12:14:14 +0800

    feat(day90/Learn_React/airbnb): 项目-Header 的封装和整体布局的搭建

---
+++ Learn_React/airbnb/src/components/app-header/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgba(0,0,0,.08);
+  height: 80px;
+`
