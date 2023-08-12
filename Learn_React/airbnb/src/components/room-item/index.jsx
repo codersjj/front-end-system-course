b2de87f509a7efbddc7157a78c68364fa1c72f05
codersjj <shanewestlife@outlook.com>
2023-08-12 16:26:34 +0800

    feat(day90/Learn_React/airbnb): 项目-首页-房间 item 的具体布局展示

--- Learn_React/airbnb/src/components/room-item/index.jsx
+++ Learn_React/airbnb/src/components/room-item/index.jsx
@@ -6,7 +6,15 @@ const RoomItem = memo((props) => {
   const { itemData } = props
 
   return (
-    <ItemWrapper>{itemData.name}</ItemWrapper>
+    // https://styled-components.com/releases#v5.1.0:~:text=Note%20the%20dollar%20sign%20(%24)%20prefix%20on%20the%20prop%3B%20this%20marks%20it%20as%20transient%20and%20styled%2Dcomponents%20knows%20not%20to%20add%20it%20to%20the%20rendered%20DOM%20element%20or%20pass%20it%20further%20down%20the%20component%20hierarchy.
+    <ItemWrapper $verifyColor={itemData.verify_info.text_color || '#39576a'}>
+      <div className="cover">
+        <img src={itemData.picture_url} alt="cover" />
+      </div>
+      <p className="desc">{itemData.verify_info.messages.join('·')}</p>
+      <p className="name">{itemData.name}</p>
+      <p className="price">¥{itemData.price}/晚</p>
+    </ItemWrapper>
   )
 })
 
--- Learn_React/airbnb/src/components/room-item/style.js
+++ Learn_React/airbnb/src/components/room-item/style.js
@@ -1,7 +1,49 @@
 import { styled } from "styled-components";
 
 export const ItemWrapper = styled.div`
+  margin-bottom: 20px;
   box-sizing: border-box;
   width: 25%;
   padding: 0 8px;
+
+  .cover {
+    position: relative;
+    border-radius: 3px;
+    /* 利用 padding-top 百分比值的特性，设置 padding-top 百分比值，作为图片的展示区域，解决图片之间高度不一致的问题 */
+    /* margin-top、margin-bottom、padding-top、padding-bottom，这些属性设置百分比值时，相对的是包含块的宽度，而不是包含块的高度 */
+    padding-top: 66.66%;
+    overflow: hidden;
+
+    img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      /* 绝对定位元素的宽高百分比值相对于其最邻近定位祖先元素（如果不是 inline box）的 padding edge 进行计算 https://drafts.csswg.org/css-sizing/#preferred-size-properties
+      https://drafts.csswg.org/css-position-3/#def-cb */
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  .desc {
+    margin: 10px 0 5px;
+    font-size: 12px;
+    font-weight: 700;
+    color: ${props => props.$verifyColor};
+  }
+
+  .name {
+    font-size: 16px;
+    font-weight: bold;
+    /* https://developer.mozilla.org/en-US/docs/Web/CSS/-webkit-line-clamp#truncating_a_paragraph */
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 2;
+    overflow: hidden;
+  }
+
+  .price {
+    margin-top: 8px;
+    font-size: 12px;
+  }
 `
